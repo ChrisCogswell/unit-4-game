f07639e8fcf6b09a9893cbcf1b5b88fad4e5cbbc
ChrisCogs <ccogs@yahoo.com>
2019-03-15 20:40:57 -0400

    Added total score boxes and some starter logic to get my bearings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,24 @@
 
+var targetNumber = 55;
+
+$("#target-number").text(targetNumber);
+
+var numberOptions = [10, 11];
+var increment = numberOptions[Math.round(Math.random())];
+
+var counter = 0;
+$(".gem-pics").on("click", function(){
+
+    counter += increment;
+    alert("Your new score is " + counter);
+
+    if (counter === targetNumber) {
+
+    alert("you win");
+    }
+
+    else if (counter > targetNumber){
+        alert("you lose");
+    }
+});
+
