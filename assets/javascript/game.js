52a0db49a00fcd75a52f106a9b14cfd6e7b6a3b5
ChrisCogs <ccogs@yahoo.com>
2019-03-17 17:35:07 -0400

    Added randomized gem values, but still need tweaking. Pre-function build

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,122 @@
+$(document).ready(function() { 
+
 // Global Variables
 
 var counter = 0;
 var wins = 0;
 var losses = 0;
 
-
 // Target Number behavior
 
-var targetNumber = 42 + Math.floor(Math.random() * 65);
+var targetNumber = 19 + Math.floor(Math.random() * 101);
 
 $("#target-number").text(targetNumber);
 
 // Variables for my crystal values
 
-var numberOptions = [4, 6, 9, 10];
-// var increment = numberOptions[Math.round(Math.random())];
+var blueGemValue = 2 + Math.floor(Math.random() * 3);
+var redGemValue = 5 + Math.floor(Math.random() * 3);        
+var greenGemValue = 7 + Math.floor(Math.random() * 4);
+var purpleGemValue = 10 + Math.floor(Math.random() * 4); 
 
-for (var i = 0; i < numberOptions.length; i++) {
+console.log(blueGemValue, redGemValue, greenGemValue, purpleGemValue);
 
-}
 
-$(".gem-pics").on("click", function(){
 
+$("#blue-gem").on("click", function(){
+
+
+    var increment = blueGemValue;
+        
     counter += increment;
     $("#score-number").text(counter);
 
-    // need to create a for loop somewhere
 
     if (counter === targetNumber) {
 
     alert("you win");
+    wins++;
+    $("#wins-text").text(wins);
     }
 
     else if (counter > targetNumber){
-        alert("you lose");
+    alert("you lose");
+    losses++;
+    $("#lose-text").text(losses);
     }
+
+    
 });
 
+$("#red-gem").on("click", function(){
+
+    var increment = redGemValue;
+
+    counter += increment;
+    $("#score-number").text(counter);
+
+
+    if (counter === targetNumber) {
+
+    alert("you win");
+    wins++;
+    $("#wins-text").text(wins);
+    }
+
+    else if (counter > targetNumber){
+    alert("you lose");
+    losses++;
+    $("#lose-text").text(losses);
+    }
+
+    
+});
+
+$("#green-gem").on("click", function(){
+
+    var increment = greenGemValue;
+
+    counter += increment;
+    $("#score-number").text(counter);
+
+
+    if (counter === targetNumber) {
+
+    alert("you win");
+    wins++;
+    $("#wins-text").text(wins);
+    }
+
+    else if (counter > targetNumber){
+    alert("you lose");
+    losses++;
+    $("#lose-text").text(losses);
+    }
+
+    
+});
+
+$("#purple-gem").on("click", function(){
+
+    var increment = purpleGemValue;
+
+    counter += increment;
+    $("#score-number").text(counter);
+
+
+    if (counter === targetNumber) {
+
+    alert("you win");
+    wins++;
+    $("#wins-text").text(wins);
+    }
+
+    else if (counter > targetNumber){
+    alert("you lose");
+    losses++;
+    $("#lose-text").text(losses);
+    }
+
+    
+});
+});
