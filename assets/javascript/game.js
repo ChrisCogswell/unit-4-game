3d1081e7d7538439c8d32cead6442d2375942f04
ChrisCogs <ccogs@yahoo.com>
2019-03-16 21:50:47 -0400

    Added gem id's, sorted javascript, made target number random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,31 @@
+// Global Variables
 
-var targetNumber = 55;
+var counter = 0;
+var wins = 0;
+var losses = 0;
+
+
+// Target Number behavior
+
+var targetNumber = 42 + Math.floor(Math.random() * 65);
 
 $("#target-number").text(targetNumber);
 
-var numberOptions = [10, 11];
-var increment = numberOptions[Math.round(Math.random())];
+// Variables for my crystal values
 
-var counter = 0;
+var numberOptions = [4, 6, 9, 10];
+// var increment = numberOptions[Math.round(Math.random())];
+
+for (var i = 0; i < numberOptions.length; i++) {
+
+}
+
 $(".gem-pics").on("click", function(){
 
     counter += increment;
-    alert("Your new score is " + counter);
+    $("#score-number").text(counter);
+
+    // need to create a for loop somewhere
 
     if (counter === targetNumber) {
 
