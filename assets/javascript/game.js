7510016058c83b2890af57954d8f527bfef36f89
ChrisCogs <ccogs@yahoo.com>
2019-03-17 19:13:59 -0400

    Added a reset function, but i'm having complications with the total score resetting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,39 @@ var counter = 0;
 var wins = 0;
 var losses = 0;
 
+$("#score-number").text(counter);
+
 // Target Number behavior
-
 var targetNumber = 19 + Math.floor(Math.random() * 101);
-
 $("#target-number").text(targetNumber);
 
 // Variables for my crystal values
 
-var blueGemValue = 2 + Math.floor(Math.random() * 3);
-var redGemValue = 5 + Math.floor(Math.random() * 3);        
-var greenGemValue = 7 + Math.floor(Math.random() * 4);
-var purpleGemValue = 10 + Math.floor(Math.random() * 4); 
+// function numberRandomizer(){
+    
+    var blueGemValue = 2 + Math.floor(Math.random() * 3);
+    var redGemValue = 5 + Math.floor(Math.random() * 3);        
+    var greenGemValue = 7 + Math.floor(Math.random() * 4);
+    var purpleGemValue = 10 + Math.floor(Math.random() * 4); 
+    // };
+    
+    console.log(blueGemValue, redGemValue, greenGemValue, purpleGemValue);
 
-console.log(blueGemValue, redGemValue, greenGemValue, purpleGemValue);
+    function resetGame() {
+        var counter = 0;
+        var targetNumber = 19 + Math.floor(Math.random() * 101);
 
+        $("#target-number").text(targetNumber);
+        $("#score-number").text(counter);
+      }
 
+    
+    
+    
+//     $("#start-box").on("click", function(){
+// });
+
+// Blue Gem
 
 $("#blue-gem").on("click", function(){
 
@@ -33,20 +50,19 @@ $("#blue-gem").on("click", function(){
 
 
     if (counter === targetNumber) {
-
-    alert("you win");
-    wins++;
-    $("#wins-text").text(wins);
+     wins++;
+     $("#wins-text").text(wins);
+     resetGame();
     }
 
     else if (counter > targetNumber){
-    alert("you lose");
-    losses++;
-    $("#lose-text").text(losses);
+     losses++;
+     $("#lose-text").text(losses);
+     resetGame();
     }
-
-    
 });
+
+// Red Gem
 
 $("#red-gem").on("click", function(){
 
@@ -57,20 +73,19 @@ $("#red-gem").on("click", function(){
 
 
     if (counter === targetNumber) {
-
-    alert("you win");
-    wins++;
-    $("#wins-text").text(wins);
+     wins++;
+     $("#wins-text").text(wins);
+     resetGame();
     }
 
     else if (counter > targetNumber){
-    alert("you lose");
-    losses++;
-    $("#lose-text").text(losses);
+     losses++;
+     $("#lose-text").text(losses);
+     resetGame();
     }
-
-    
 });
+
+// Green Gem
 
 $("#green-gem").on("click", function(){
 
@@ -81,20 +96,19 @@ $("#green-gem").on("click", function(){
 
 
     if (counter === targetNumber) {
-
-    alert("you win");
-    wins++;
-    $("#wins-text").text(wins);
+     wins++;
+     $("#wins-text").text(wins);
+     resetGame();
     }
 
     else if (counter > targetNumber){
-    alert("you lose");
-    losses++;
-    $("#lose-text").text(losses);
+     losses++;
+     $("#lose-text").text(losses);
+     resetGame();
     }
-
-    
 });
+
+// Purple Gem
 
 $("#purple-gem").on("click", function(){
 
@@ -105,18 +119,22 @@ $("#purple-gem").on("click", function(){
 
 
     if (counter === targetNumber) {
-
-    alert("you win");
-    wins++;
-    $("#wins-text").text(wins);
+     wins++;
+     $("#wins-text").text(wins);
+     resetGame();
     }
 
     else if (counter > targetNumber){
-    alert("you lose");
-    losses++;
-    $("#lose-text").text(losses);
+     losses++;
+     $("#lose-text").text(losses);
+     resetGame();
     }
-
-    
 });
+
+// Reset Button
+
+// $("#reset-box").on("click", function() {
+//     resetGame();
+// });
+
 });
